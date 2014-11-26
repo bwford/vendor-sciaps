f494163b2d55d2dbd1d0be1457621a64a3e5417d
paul <psoucy@sciaps.com>
2014-11-26 15:16:38 -0500

    fixup the AOSP makefiles for kernel stuff

--- kernel.mk
+++ kernel.mk
@@ -1,8 +1,7 @@
 .PHONY: kernel cleankernel kernelconfig kernelmodules
 
 KERNEL_UIMAGE := $(KERNEL_DIR)/arch/arm/boot/uImage
-PVRSGX_MODULE := out/target/product/$(TARGET_DEVICE)/target/kbuild/pvrsrvkm_sgx540_120.ko
-KSP5012_VERSIONCHECK_MODULE := kernel/arch/arm/mach-omap2/board-omap4ksp5012-version.ko
+
 
 WIFI_DRIVERS := \
 	/net/wireless/cfg80211.ko \
@@ -30,31 +29,20 @@ kernelmodules: $(KERNEL_DIR)/.config
 	cd $(KERNEL_DIR) && \
 	make CROSS_COMPILE=arm-eabi- ARCH=arm modules -j8
 
-$(KERNEL_WIRELESS_MODULES): kernelmodules
+
+kernel: $(KERNEL_UIMAGE) $(KERNEL_MODULES)
+
+cleankernel:
+	rm -f $(KERNEL_UIMAGE) $(KERNEL_MODULES)
+	cd $(KERNEL_DIR) && make CROSS_COMPILE=arm-eabi- ARCH=arm clean
+
+
+############### KSP5012 version check ########################################################
+
+KSP5012_VERSIONCHECK_MODULE := kernel/arch/arm/mach-omap2/board-omap4ksp5012-version.ko
 
 $(KSP5012_VERSIONCHECK_MODULE): kernelmodules
 
-sgx/eurasia_km/INSTALL:
-	mkdir -p sgx
-	cd sgx && tar xzf ../device/ti/proprietary-open/omap4/sgx_src/eurasia_km.tgz
-	touch sgx/eurasia_km/INSTALL
-
-
-$(PVRSGX_MODULE): $(KERNEL_UIMAGE) sgx/eurasia_km/INSTALL
-	cd sgx/eurasia_km/eurasiacon/build/linux2/omap4430_android && \
-	export KERNELDIR=$(abspath $(KERNEL_DIR)) && \
-	make ARCH=arm CROSS_COMPILE=arm-eabi- TARGET_PRODUCT="blaze_tablet" BUILD=release TARGET_SGX=540 PLATFORM_VERSION=4.1 clean && \
-	make ARCH=arm CROSS_COMPILE=arm-eabi- TARGET_PRODUCT="blaze_tablet" BUILD=release TARGET_SGX=540 PLATFORM_VERSION=4.1 -j8
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := pvrsrvkm_sgx540_120.ko
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_PATH := $(TARGET_OUT)/lib/modules
-LOCAL_SRC_FILES := $(PVRSGX_MODULE)
-include $(BUILD_SYSTEM)/base_rules.mk
-$(LOCAL_BUILT_MODULE) : $(LOCAL_SRC_FILES) | $(ACP)
-	$(transform-prebuilt-to-target)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := board-omap4ksp5012-version.ko
@@ -66,28 +54,98 @@ include $(BUILD_SYSTEM)/base_rules.mk
 $(LOCAL_BUILT_MODULE) : $(LOCAL_SRC_FILES) | $(ACP)
 	$(transform-prebuilt-to-target)
 
+############### Wifi Driver ###################################################################
 
-.PHONY: wirelesskm
 EXTERNAL_WIRELESS_DIR := hardware/ti/wlan/mac80211/compat_wl12xx
 EXTERNAL_WIRELESS_MODULES := $(addprefix $(EXTERNAL_WIRELESS_DIR),$(WIFI_DRIVERS))
 
-wirelesskm: $(KERNEL_UIMAGE)
+EXTERN_WIFI_MAKE := out/target/product/$(TARGET_DEVICE)/_externwifi
+
+$(EXTERN_WIFI_MAKE): $(KERNEL_UIMAGE)
 	cd $(EXTERNAL_WIRELESS_DIR) && \
 	export KERNEL_DIR=$(abspath $(KERNEL_DIR)) && \
 	export KLIB=$(abspath $(KERNEL_DIR)) && \
 	export KLIB_BUILD=$(abspath $(KERNEL_DIR)) && \
 	make ARCH=arm CROSS_COMPILE=arm-eabi-
+	touch $(EXTERN_WIFI_MAKE)
 
-$(EXTERNAL_WIRELESS_MODULES): wirelesskm
+$(EXTERNAL_WIRELESS_MODULES): $(EXTERN_WIFI_MAKE)
 
-kernel: $(KERNEL_UIMAGE) $(KERNEL_MODULES)
-
-cleankernel:
-	rm -f $(KERNEL_UIMAGE) $(KERNEL_MODULES)
-	cd $(KERNEL_DIR) && make CROSS_COMPILE=arm-eabi- ARCH=arm clean
+WIRELESS_DIR := $(EXTERNAL_WIRELESS_DIR)
+#WIRELESS_DIR := $(KERNEL_DIR)
 
 
-############### BareBox MLO ###############
+include $(CLEAR_VARS)
+LOCAL_MODULE := mac80211.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_PATH := $(TARGET_OUT)/lib/modules
+LOCAL_SRC_FILES := $(WIRELESS_DIR)/net/mac80211/mac80211.ko
+include $(BUILD_SYSTEM)/base_rules.mk
+$(LOCAL_BUILT_MODULE) : $(LOCAL_SRC_FILES) | $(ACP)
+	$(transform-prebuilt-to-target)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := cfg80211.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_PATH := $(TARGET_OUT)/lib/modules
+LOCAL_SRC_FILES := $(WIRELESS_DIR)/net/wireless/cfg80211.ko
+include $(BUILD_SYSTEM)/base_rules.mk
+$(LOCAL_BUILT_MODULE) : $(LOCAL_SRC_FILES) | $(ACP)
+	$(transform-prebuilt-to-target)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := wl12xx.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_PATH := $(TARGET_OUT)/lib/modules
+LOCAL_SRC_FILES := $(WIRELESS_DIR)/drivers/net/wireless/wl12xx/wl12xx.ko
+include $(BUILD_SYSTEM)/base_rules.mk
+$(LOCAL_BUILT_MODULE) : $(LOCAL_SRC_FILES) | $(ACP)
+	$(transform-prebuilt-to-target)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := wl12xx_sdio.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_PATH := $(TARGET_OUT)/lib/modules
+LOCAL_SRC_FILES := $(WIRELESS_DIR)/drivers/net/wireless/wl12xx/wl12xx_sdio.ko
+include $(BUILD_SYSTEM)/base_rules.mk
+$(LOCAL_BUILT_MODULE) : $(LOCAL_SRC_FILES) | $(ACP)
+	$(transform-prebuilt-to-target)
+
+
+
+
+############### Graphics Driver ###############################################################
+
+PVRSGX_MODULE := out/target/product/$(TARGET_DEVICE)/target/kbuild/pvrsrvkm_sgx540_120.ko
+
+sgx/eurasia_km/INSTALL:
+	mkdir -p sgx
+	cd sgx && tar xzf ../device/ti/proprietary-open/omap4/sgx_src/eurasia_km.tgz
+	touch sgx/eurasia_km/INSTALL
+
+
+$(PVRSGX_MODULE): $(KERNEL_UIMAGE) sgx/eurasia_km/INSTALL
+	cd sgx/eurasia_km/eurasiacon/build/linux2/omap4430_android && \
+	export KERNELDIR=$(abspath $(KERNEL_DIR)) && \
+	make ARCH=arm CROSS_COMPILE=arm-eabi- TARGET_PRODUCT="blaze_tablet" BUILD=release TARGET_SGX=540 PLATFORM_VERSION=4.1 clean && \
+	make ARCH=arm CROSS_COMPILE=arm-eabi- TARGET_PRODUCT="blaze_tablet" BUILD=release TARGET_SGX=540 PLATFORM_VERSION=4.1
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := pvrsrvkm_sgx540_120.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_PATH := $(TARGET_OUT)/lib/modules
+LOCAL_SRC_FILES := $(PVRSGX_MODULE)
+include $(BUILD_SYSTEM)/base_rules.mk
+$(LOCAL_BUILT_MODULE) : $(LOCAL_SRC_FILES) | $(ACP)
+	$(transform-prebuilt-to-target)
+
+
+############### BareBox MLO ##################################################################################
 BAREBOX_MLO_CONFIG := barebox/barebox_mlo-2013.06.0/.config
 
 $(BAREBOX_MLO_CONFIG):
@@ -105,7 +163,7 @@ barebox_mloclean:
 	make CROSS_COMPILE=arm-eabi- ARCH=arm clean
 
 
-############### BareBox ####################
+############### BareBox #####################################################################################
 BAREBOX_CONFIG := barebox/barebox-2013.06.0/.config
 
 $(BAREBOX_CONFIG):
--- wl12xx/Android.mk
+++ wl12xx/Android.mk
@@ -6,7 +6,7 @@ TARGET_OUT_WLAN_FW := $(TARGET_OUT_ETC)/firmware/ti-connectivity
 FIRMWARE_FILES := $(notdir $(shell find $(LOCAL_PATH) -iname *.bin))
 
 define prebuilt-firmware
-include $(CLEAR_VARS)
+include $$(CLEAR_VARS)
 LOCAL_MODULE := $(1)
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $$(TARGET_OUT_WLAN_FW)
@@ -17,51 +17,6 @@ include $$(BUILD_PREBUILT)
 endef
 
 $(foreach d,$(FIRMWARE_FILES),$(eval $(call prebuilt-firmware,$d)))
-
-
-WIRELESS_DIR := hardware/ti/wlan/mac80211/compat_wl12xx
-#WIRELESS_DIR := $(KERNEL_DIR)
-
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := mac80211.ko
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_PATH := $(TARGET_OUT)/lib/modules
-LOCAL_SRC_FILES := $(WIRELESS_DIR)/net/mac80211/mac80211.ko
-include $(BUILD_SYSTEM)/base_rules.mk
-$(LOCAL_BUILT_MODULE) : $(LOCAL_SRC_FILES) | $(ACP)
-	$(transform-prebuilt-to-target)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := cfg80211.ko
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_PATH := $(TARGET_OUT)/lib/modules
-LOCAL_SRC_FILES := $(WIRELESS_DIR)/net/wireless/cfg80211.ko
-include $(BUILD_SYSTEM)/base_rules.mk
-$(LOCAL_BUILT_MODULE) : $(LOCAL_SRC_FILES) | $(ACP)
-	$(transform-prebuilt-to-target)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := wl12xx.ko
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_PATH := $(TARGET_OUT)/lib/modules
-LOCAL_SRC_FILES := $(WIRELESS_DIR)/drivers/net/wireless/wl12xx/wl12xx.ko
-include $(BUILD_SYSTEM)/base_rules.mk
-$(LOCAL_BUILT_MODULE) : $(LOCAL_SRC_FILES) | $(ACP)
-	$(transform-prebuilt-to-target)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := wl12xx_sdio.ko
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_PATH := $(TARGET_OUT)/lib/modules
-LOCAL_SRC_FILES := $(WIRELESS_DIR)/drivers/net/wireless/wl12xx/wl12xx_sdio.ko
-include $(BUILD_SYSTEM)/base_rules.mk
-$(LOCAL_BUILT_MODULE) : $(LOCAL_SRC_FILES) | $(ACP)
-	$(transform-prebuilt-to-target)
 
 
 
